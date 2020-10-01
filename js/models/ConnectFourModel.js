a4acb03be79d102eae185ecbc3ad993af809ff09
Bo de Vroome <bodevroome@hotmail.com>
2020-10-01 13:54:22 +0200

    Player CHANGE

--- js/models/ConnectFourModel.js
+++ js/models/ConnectFourModel.js
@@ -14,14 +14,18 @@ export class ConnectFourModel extends EventTarget{
     }
 
     acceptMoveOn(columnId){
-       for(let row = 5; row>=0; row--){
-           if(this.values[row][columnId]===0){
-               this.values[row][columnId]= this.playerId;
+       for(let row = 5; row>=0; row--) {
+           if (this.values[row][columnId] === 0) {
+               this.values[row][columnId] = this.playerId;
+               if (this.playerId === 1) {
+                   this.playerId = 2;
+               } else {
+                   this.playerId = 1;
+               }
                break;
            }
        }
     }
-
 
     getValues(){
         return this.values;
